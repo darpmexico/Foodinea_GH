f0f84d30eced309194a6758a972a188187ee0a87
Carla Lozano <krlita_clb@hotmail.com>
2015-08-23 23:36:45 -0500

    prueba

---
+++ foodinea/application/language/ES/ES_lang_prueba1.php
@@ -0,0 +1,92 @@
+<?php
+//$lang['descripcion_tour'] ="here goes the description tour";
+$lang['cat_tour01mx']="Gastro City Tours";
+$lang['cat_tour02mx']="Tour de bares";
+$lang['cat_tour03mx']="Rutas gastronomicas";
+$lang['cat_tour04mx']="Gastronomia prehispanica";
+$lang['cat_tour05mx']="Ruta del tequila";
+$lang['cat_tour06mx']="Vacaciones gastronomicas";
+$lang['cat_tour07mx']="Degustacion y rutas de vino";
+$lang['cat_tour08mx']="Tour de gastronomia callejera";
+$lang['cat_tour09mx']="Tour del taco";
+$lang['cat_tour10mx']="Ruta del tequila";
+$lang['cat_tour11mx']="Cursos y clases de cocina";
+$lang['cat_tour12mx']="Vacaciones gastronomicas";
+
+// TOUR1 MEXICO
+//nombre
+$lang['nam_tour1mx']="Choco Tour. La ruta del cacao y del chocolate en el Centro Histórico";
+//duracion
+$lang['dur_tour1mx']="1 dia";
+//idioma
+$lang['idi_tour1mx']="Español";
+//confirmacion
+$lang['conf_tour1mx']="Inmediata";
+//ubicacion
+$lang['ubi_tour1mx']="Chiapas Centro";
+//punto de encuentro
+$lang['pnt_tour1mx']="Detalles vía e-mail";
+//destacado
+$lang['dest_tour1mx']="Aprende a preparar un latte como  baristaprofesional.
+Aprende a decorar tu taza de latte con figuras y motivos de tu elección.
+Degusta tu  bebida."; 
+//descripcion
+$lang['desc_tour1mx']="Aprende a crear arte en una taza de café, como un experto barista, a la temperatura correcta y con las cantidades correctas.
+Es el toque final de la taza de café el que le suma arte.";
+//incluye
+$lang['inc_tour1mx']="Clase dinamica de preparación y decoracoión de latte con café chiapaneco.
+Degustación de latte chiapaneco.
+Transporte redondo a clase.";
+//mas informacion
+$lang['inf_tour1mx']="El precio del tour no incluye transporte desde/ a hotel o domicilio privado.
+Apto solo para todas las edades.
+Te recomendamos reservar con anticipación.
+¿Requieres un tour para grupo ? Envianos un mail a hi@foodinea.com , nos dará gusto ayudarte";
+//precio
+$lang['prec_tour1mx']="50 USD";
+//imagen
+$lang['img_tour1mx']=""; //RUTA DE LA IMAGEN 1mex.jpg
+
+// TOUR2 MEXICO
+$lang['nam_tour2mx']="Tour de Cantinas Ciudad de México";
+$lang['dur_tour2mx']="3 horas";
+$lang['idi_tour2mx']="Ingles, Español";
+$lang['conf_tour2mx']="Inmediata";
+$lang['ubi_tour2mx']="Centro histórico, Ciudad de México";
+$lang['pnt_tour2mx']="Detalles vía e-mail";
+$lang['dest_tour2mx']="Conoce la primera cantina de México, historia y cultura.
+Pasa un rato de diversión en ambiente familiar.
+Disfruta de un buffet típico mexicano en el restaurante Gallo de Oro, uno de los más antiguos de la ciudad."; 
+$lang['desc_tour2mx']="Las cantinas además de ser templos a Dionisio, son Templos de la Historia y hoy en día a la familia. Forman parte de nuestra identidad como mexicanos ya que después de tantos años muchos relatos se guardan al entrar a esos “Templos de dos Hojas”, como Salvador Novo las llamaba. Hoy en día dentro de ellas se puede convivir de manera única: sin complejos, miramientos, ni clases sociales, y por unas horas el rico, el pobre, el obrero, el universitario, el artista, el político, brindan se divierten y crean una camaradería, la cual se puede resolver cualquier tipo de problema (social, político, deportivo, humano, económico, etc.)";
+$lang['inc_tour2mx']="Buffet  tradicional mexicano en  restaurante el Gallo de Oro.
+Caminata de aproximadamente 3 horas por  bares y cantinas de la época 
+Visita a las siguientes cantinas:
+•Bar La Opera
+•Cantina El Gallo de Oro
+•Cantina La Faena
+•Bar Mancera
+•Bar Gante";
+$lang['inf_tour2mx']="El precio del tour no incluye transporte.
+El primer  trago obligatorio en la primera cantina, y es pagado por el cliente, en las demás el consumo  de alcohol o bebidas es opcional.
+Las bebidas corren por cuenta del cliente.";
+$lang['prec_tour2mx']="22 USD";
+$lang['img_tour2mx']=""; //RUTA DE LA IMAGEN 2mex.jpg
+
+// TOUR3 MEXICO
+$lang['nam_tour3mx']="";
+$lang['dur_tour3mx']="";
+$lang['idi_tour3mx']="";
+$lang['conf_tour3mx']="Inmediata";
+$lang['ubi_tour3mx']="Centro histórico, Ciudad de México";
+$lang['pnt_tour3mx']="Detalles vía e-mail";
+$lang['dest_tour3mx']="Conoce la primera cantina de México, historia y cultura.
+Pasa un rato de diversión en ambiente familiar.
+Disfruta de un buffet típico mexicano en el restaurante Gallo de Oro, uno de los más antiguos de la ciudad."; 
+$lang['desc_tour3mx']="";
+$lang['inc_tour3mx']="";
+$lang['inf_tour3mx']="";
+$lang['prec_tour3mx']="";
+$lang['img_tour3mx']=""; //RUTA DE LA IMAGEN 2mex.jpg
+
+
+?>
