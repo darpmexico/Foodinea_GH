eb7c27f67e4c0532bd834c32265f54c2f9f4024e
Carla Lozano <krlita_clb@hotmail.com>
2015-08-24 01:10:12 -0500

    Tours Mexico
    Este es el formato en el que se estan llenando las variables de los
    tours.
    EL cambio se hace sobre la ruta;
    C:\...\GitHub\Foodinea_GH\foodinea\application\language\ES
    El contenido de los tours lo obtenemos de:
    C:\...\Dropbox\FOODINEA\Documentos Funcionales\CONTENIDO\TOURS

--- foodinea/application/language/ES/ES_lang_prueba1.php
+++ foodinea/application/language/ES/ES_lang_prueba1.php
@@ -44,8 +44,6 @@ Te recomendamos reservar con anticipación.
 ¿Requieres un tour para grupo ? Envianos un mail a hi@foodinea.com , nos dará gusto ayudarte";
 //precio
 $lang['prec_tour1mx']="50 USD";
-//imagen
-$lang['img_tour1mx']=""; //RUTA DE LA IMAGEN 1mex.jpg
 
 // TOUR2 MEXICO
 $lang['nam_tour2mx']="Tour de Cantinas Ciudad de México";
@@ -70,23 +68,216 @@ $lang['inf_tour2mx']="El precio del tour no incluye transporte.
 El primer  trago obligatorio en la primera cantina, y es pagado por el cliente, en las demás el consumo  de alcohol o bebidas es opcional.
 Las bebidas corren por cuenta del cliente.";
 $lang['prec_tour2mx']="22 USD";
-$lang['img_tour2mx']=""; //RUTA DE LA IMAGEN 2mex.jpg
 
 // TOUR3 MEXICO
-$lang['nam_tour3mx']="";
-$lang['dur_tour3mx']="";
-$lang['idi_tour3mx']="";
+$lang['nam_tour3mx']="Guadalajara Pub Crawl";
+$lang['dur_tour3mx']="2 horas";
+$lang['idi_tour3mx']="Español";
 $lang['conf_tour3mx']="Inmediata";
-$lang['ubi_tour3mx']="Centro histórico, Ciudad de México";
-$lang['pnt_tour3mx']="Detalles vía e-mail";
-$lang['dest_tour3mx']="Conoce la primera cantina de México, historia y cultura.
-Pasa un rato de diversión en ambiente familiar.
-Disfruta de un buffet típico mexicano en el restaurante Gallo de Oro, uno de los más antiguos de la ciudad."; 
-$lang['desc_tour3mx']="";
-$lang['inc_tour3mx']="";
-$lang['inf_tour3mx']="";
-$lang['prec_tour3mx']="";
-$lang['img_tour3mx']=""; //RUTA DE LA IMAGEN 2mex.jpg
+$lang['disp_tour3mx']="Viernes";       //DISPONIBILIDAD
+$lang['ubi_tour3mx']="Guadalajara";
+$lang['pnt_tour3mx']="Rojo Café en Calle José Guadalupe Zuno Hernández 2027, Colonia Lafayette, Guadalajara, Jalisco";
+$lang['dest_tour3mx']="Visita 4 bares en una noche y bebe en cada uno de ellos.
+Disfruta de diferentes bares temáticos.
+Diviértete como mexicano, conoce gente, haz amigos.
+Enamórate de la arquitectura de la ciudad."; 
+$lang['desc_tour3mx']="Este es un tour alternativo guiado que quieren ir más allá de que un turista tradicional para empezar la mejor noche en la Colonia Americana, barrio artístico por excelencia de Guadalajara, donde probarás diferentes opciones internacionales para divertirte.
+Entre las degustaciones, encontrarás una vista complete de las calles de la colonia Americana. Nuestros guías expertos compartirán su conocimiento de arquitectura, lugares escondidos y excitantes opciones de entretenimiento.
+Café: Un espacio de expresión artístico único en la ciudad.
+Agüita: Un bar donde encontrarás mezcales y cocina fusión.
+Pub: Un ambiente “gemutlichkeit” para degustar cervezas de todo el mundo, más de 200 marcas.
+El Callejón De Los Rumberos: El lugar donde la música y el sabor afro caribeño se apoderan de la colonia americana.";
+$lang['inc_tour3mx']="Recorrido guiado.
+4 bebidas alcohólicas y botanas";
+$lang['inf_tour3mx']="El precio del tour no incluye transporte.
+Apto para mayores de 18 años.
+Apto para personas con capacidades diferentes.
+No incluye consumo adicional a las degustaciones.
+Reserva con al menos 5 días  anticipación.";
+$lang['prec_tour3mx']="19 USD";
+
+// TOUR4 MEXICO
+$lang['nam_tour4mx']="Tour Fabrica Del chocolate";
+$lang['dur_tour4mx']="60 minutos";
+$lang['idi_tour4mx']="Inglés, Español";
+$lang['conf_tour4mx']="12 horas";
+$lang['ubi_tour4mx']="Cozumel, Quintana Roo";
+$lang['pnt_tour4mx']="Detalles vía e-mail";
+$lang['dest_tour4mx']="Conoce las herramientas usadas por los mayas para preparar la bebida sagrada ´´El chocolate´´
+Prepara tu propio chocolate.
+Prueba los diferentes sabores del cacao durante su proceso."; 
+$lang['desc_tour4mx']="Diviértete disfrutando un  tour en la fábrica del chocolate en la isla de Cozumel, aprende de la historia de la bebida de dioses ´´El chocolate´´  que fue consumido por los mayas durante su imperio. Conoce las herramientas  que los mayas usaban para preparar esta bebida, prueba diferentes sabores de cacao y prepara tu propio chocolate.";
+$lang['inc_tour4mx']="Boleto de entrada a la fábrica de chocolate.
+Recorridos guiados en inglés y español.
+•Degustaciones de chocolate.";
+$lang['inf_tour4mx']="El precio del tour no incluye  transporte  desde/a hotel o domicilio.
+Apto para personas con capacidades diferentes
+Disponible lunes a domingo  con salidas a 11:00 am -1pm";
+$lang['prec_tour4mx']="20 USD";
+
+// TOUR5 MEXICO
+$lang['nam_tour5mx']="Tour del mole poblano";
+$lang['dur_tour5mx']="12 horas";
+$lang['idi_tour5mx']="Español";
+$lang['conf_tour5mx']="12 horas";
+$lang['ubi_tour5mx']="Puebla, Puebla";
+$lang['disp_tour3mx']="Viernes";       //DISPONIBILIDAD
+$lang['pnt_tour5mx']="Detalles vía e-mail";
+$lang['dest_tour5mx']="Vive un viaje gastronómico, turístico, cultural y en Puebla de los Ángeles, Ciudad Patrimonio de la Humanidad.
+Disfruta de un desayuno típico poblano en un mercado mexicano
+Recorre el centro de la ciudad de los Ángeles y el museo de la Revolución Mexicana
+Come en uno de los hoteles más emblemáticos de la ciudad."; 
+$lang['desc_tour5mx']="La Ruta Gastronómica del Mole en el estado de Puebla es una experiencia gastronómica, turística y de aprendizaje dedicada a que los aventureros e interesados vivan y disfruten la riqueza cultural y turística del estado de Puebla, que siempre deleita a los visitante con su rica comida.
+Los ingredientes más representativos de la región la canela, pimienta, chile y dulce sabores que se mezclan para dar paso a esa exquisita gastronomía llena olores y colores, A través de un recorrido con duración de un día, el aventurero o aventurera estudiante, vivirá la historia y cultura del estado en un recorrido que lo llevara al centro histórico de la ciudad en donde se recorrerán museos y calles. Se visitará el hotel La Purificadora que es la expresión de una fábrica purificadora de agua del siglo XIX, su restaurante tiene una belleza arquitectónica contemporánea; diseñado por el arquitecto Ricardo Legorreta con mesas compartidas que fueron elaboradas con la madera apolillada encontrada en la antigua Purificadora. En este espacio de gran belleza se llevará a cabo un taller vivencial en donde el aventurero podrá observar como es la cocina tradicional del mole en metate, así como conocer sus ingredientes y fundamentos históricos.";
+$lang['inc_tour5mx']="Recorridos guiados.
+Desayuno típico poblano.
+Comida en el hotel la purificadora.
+Transporte de/a punto de encuentro Ciudad de México-Puebla-Ciudad de México.";
+$lang['inf_tour5mx']="El precio del tour no incluye  transporte  desde/a hotel o domicilio.
+Apto para personas con capacidades diferentes.
+Importante reservar con 5 días de anticipación.
+Disponible únicamente en viernes.";
+$lang['prec_tour5mx']="116 USD";
+
+// TOUR6 MEXICO
+$lang['nam_tour6mx']="Tour del café";
+$lang['dur_tour6mx']="4 horas";
+$lang['idi_tour6mx']="Ingles, Español";
+$lang['conf_tour6mx']="24 horas";
+$lang['ubi_tour6mx']="Coatepec, Veracruz";
+$lang['pnt_tour6mx']="Finca de Don Silvano Khalid 13 Fracc. Villa Pixquiac Km. 7 Carret. Antigua Xalapa-Coatepec Coatepec, Veracruz";
+$lang['dest_tour6mx']="Recorre el cafetal dentro del bosque de niebla.
+Conoce la preparación artesanal  del café.
+Degusta una variedad de café."; 
+$lang['desc_tour6mx']="Viva una mágica experiencia descubriendo todo lo que hay alrededor de un cafetal dentro del Bosque de Niebla y el mundo de esta aromática bebida; entrando en contacto con la naturaleza, la historia de la Finca de Don Silvano, el proceso artesanal de este producto y la trayectoria del café desde su origen hasta este hermoso lugar. Finaliza con una degustación que además de enseñarte a reconocer una buena taza de café, te hará vivir increíbles sensaciones, que te dejarán un gran sabor de boca.";
+$lang['inc_tour6mx']=" Bienvenida a la Finca de Don Silvano con degustación de café.
+Historia de la Finca: Explicación del proceso artesanal del Café.
+Vivero de Helechos Arborescentes, con un helecho de 300 años de vida.
+Explicación del Cultivo del Café.
+Huertos urbanos: Explicación del Vivero de Hortalizas y Hierbas de Olor.
+Caminata en Sendero temático con explicación de Cultura e Historia del Café.
+Vivero de Epífitas.
+Cata Sensorial de Café con maridaje.";
+$lang['inf_tour6mx']="El precio del tour no incluye  transporte.
+Niños menores de 4 años no pagan
+Actividad ligera.
+Disponible Lunes a domingo  con salidas a 11:00 am -1pm.";
+$lang['prec_tour6mx']="35 USD";
+
+// TOUR7 MEXICO
+$lang['nam_tour7mx']="Tour mercados DF";
+$lang['dur_tour7mx']="6 horas";
+$lang['idi_tour7mx']="Ingles, Español";
+$lang['conf_tour7mx']="24 horas";
+$lang['ubi_tour7mx']="Ciudad de México";
+$lang['pnt_tour7mx']="Detalles vía e-mail";
+$lang['dest_tour7mx']="Visita  cuatro mercados tradicionales mexicanos, incluyendo el mercado más grande de la ciudad.
+Pasea a través del mercado de  productos gourmet más grande le mundo
+Degusta platillos y bebidas mexicanas."; 
+$lang['desc_tour7mx']="Durante este recorrido visitaremos cuatro mercados especiales, incluyendo Mercado Jamaica el mercado famoso por sus hermosas flores y frutas de colores. También vamos a ir a La Merced, el mercado más grande de la ciudad que primero Comenzó su actividad en la época prehispánica. Luego nos dirigiremos a San Juan, el mercado famoso por sus productos de comida gourmet y, finalmente, el Mercado de Sonora, que es famoso por sus productos relacionados con la brujería y la medicina a base de hierbas y animales exóticos.
+El domingo cambiamos San Juan de La Lagunilla, un mercado que es famoso por las antigüedades y deliciosas micheladas.";
+$lang['inc_tour7mx']="Transporte a través de metro.
+Recorridos guiados en ingles y español.
+Alimentos y bebidas.";
+$lang['inf_tour7mx']="El precio del tour no incluye  transporte  desde/a hotel o domicilio.
+No incluye comida adicional a la asignada en las degustaciones.
+Disponible Lunes a  viernes. Salidas 10:30 am";
+$lang['prec_tour7mx']="100 USD";
+
+// TOUR8 MEXICO
+$lang['nam_tour8mx']="Tour Gastronomía Tlaxcalteca";
+$lang['dur_tour8mx']="7 horas";
+$lang['idi_tour8mx']="Español";
+$lang['conf_tour8mx']="12 horas";
+$lang['ubi_tour8mx']="Tlaxcala";
+$lang['pnt_tour8mx']="Centro de Tlaxcala. Detalles vía e-mail";
+$lang['dest_tour8mx']="Recorre el Centro Histórico Tlaxcalteca y disfruta su arquitectura colonial.
+Visita un taller de cerveza artesanal.
+Pasea por las magueyeras y conoce la elaboración del la bebida de los dioses mayas 'El pulque'.
+Disfruta de una comida tradicional mexicana en una hacienda."; 
+$lang['desc_tour8mx']="La fusión de pasado y presente en la gastronomía del estado de Tlaxcala hacen que sea única en el país. Su influencia prehispánica, enriquecida con importantes ingredientes españoles, da forma a una multitud de sabores, aromas y colores que son una auténtica delicia. Se trata de una de las riquezas del patrimonio cultural de México. Adéntrate en un tour  gastro- histórico para disfrutar de las costumbres  culinarias de la región. El itinerario comprende las siguientes actividades:
+Bienvenida en Tlaxcala – Encuentro con los guías.
+Recorrido Centro Histórico.
+Visita a Taller de Cerveza Artesanal.
+Llegada a Hacienda Pulquera.
+Recorrido por las Magueyeras para ver como se capa un maguey, como se extrae el aguamiel, el mixiote y el ixtle.
+En el Tinacal Mitos, versos y realidades del Pulque.
+Comida en la Hacienda.";
+$lang['inc_tour8mx']="Entrada a atracciones.
+Transporte centro-tour-centro.
+Recorridos guiados.
+Alimentos.
+Seguro de viajero.";
+$lang['inf_tour8mx']="El precio del tour no incluye bebidas alcohólicas ni traslado a  hotel o domicilio particular.
+Ideal para todas las edades.";
+$lang['prec_tour8mx']="150 USD";
+
+// TOUR9 MEXICO
+$lang['nam_tour9mx']="Circuito tequila Guanajuatense";
+$lang['dur_tour9mx']="7 horas";
+$lang['idi_tour9mx']="Ingles, Español";
+$lang['conf_tour9mx']="12 horas";
+$lang['ubi_tour9mx']="Guanajuato";
+$lang['pnt_tour9mx']="Detalles vía e-mail";
+$lang['dest_tour9mx']="Visita la fabrica Tequilera Corralejo y bodegón Dolce Vita.
+Deléitate  con una comida tradicional mexicana.
+Observa la jima de tequila en el campo de agave."; 
+$lang['desc_tour9mx']="Guanajuato, con denominación de origen, te invita a vivir en sus campos  de agave una experiencia de momentos increíbles con sabor  a  tequila y emociones que ponen a  gozar tus  sentidos  entre copas y cazuelas.
+Visita a  Fabrica Tequilera  Corralejo una de las productoras de los mejores tequilas del país, el Bodegón de la Dolce Vita, Palacio del Tiempos. Conoce el proceso de elaboración del tequila desde su base  observando   la Jima en el  Campo de Agave, termina tu día disfrutando de comida regional mexicana.";
+$lang['inc_tour9mx']="Visita a atracciones.
+Comida típica mexicana.";
+$lang['inf_tour9mx']="El precio del tour nocturno no incluye alcohol ni transporte privado.";
+$lang['prec_tour9mx']="150 USD";
+
+// TOUR10 MEXICO
+$lang['nam_tour10mx']="Los mil sabores del mole: Gastronomía Oaxaqueña";
+$lang['dur_tour10mx']="7 días";
+$lang['idi_tour10mx']="Ingles, Español";
+$lang['conf_tour10mx']="12 horas";
+$lang['ubi_tour10mx']="Oaxaca";
+$lang['pnt_tour10mx']="Aeropuerto Internacional de Oaxaca";
+$lang['dest_tour10mx']="Saborea los sabores de la cocina tradicional y la fusión de Oaxaca.
+Disfrute de una experiencia cuidadosamente diseñado para ofrecerle una mezcla de la cultura, la gastronomía y el arte.
+Hospédate  en un hotel boutique con estilo exquisito estilo oaxaqueño.
+Abraza la magia de Oaxaca a través de su historia y cultura mística."; 
+$lang['desc_tour10mx']="Itinerario
+Día 1
+Pasamos por ti en el Aeropuerto Internacional de Oaxaca; de allí lo llevaremos al Hotel Boutique donde se alojará, situada en el corazón del centro de la ciudad de Oaxaca, a solo unas cuadras de la plaza principal de Oaxaca, el Zócalo. Después de registrarnos, nos vamos a salir a las calles para un paseo introductorio de la ciudad de Oaxaca.
+Día 2
+• Desayuno en el hotel.
+• Introducción a la historia y la cultura de Oaxaca y su comida.
+• Visita a la bulliciosa Mercado de Abastos, el mercado más grande de Oaxaca. ¿Dónde vamos a tejemos nuestro camino a través de las frutas y verduras frescas, paquetes de chiles, puestos de venta de flores frescas, y mucho más!
+• chocolate! Después del mercado, vamos a hacer nuestro camino a uno de los molinos de chocolate, donde se llega a ver el proceso de elaboración del chocolate oaxaqueño.
+• Vamos a descansar el día en Mezcaloteca, una sala de degustación de mezcal, y disfrutar de algunos de los mejores mezcal de la ciudad.
+Día 3
+• Desayuno en el hotel.
+• Nuestra primera clase de cocina! En nuestra primera clase de cocina vamos a aprender a hacer dulces típicos oaxaqueños, conocidos como Dulces Típicos, que incluían ñame confitadas, tartas de coco, guayaba flanes y mucho más!
+• Cena en Biznaga.
+Día 4
+• Desayuno en el hotel.
+• tradicional clase de cocina zapoteca en la casa de Eugenia en Teotitlán, donde aprenderán a hacer mole y tamales hechos a mano.
+• Almuerzo en Eugenia.
+• Mientras que en Teotitlán también encontraremos un tejedor tradicional y aprendemos sobre el proceso detallado y laborioso de hacer una alfombra tejida a partir de cardado a morir para tejer.
+• Cena en Pitiona.
+Día 5
+• Desayuno en el hotel.
+• Tamal y clase de cocina atole.
+• Visita a la zona arqueológica de Monte Albán, una vez considerada la capital del pueblo zapoteca entre los años 500 aC y 800 dC y Arrazola, pueblo de la Alebrijes (tallas de madera de animales fantásticos).
+• Cena en Casa Oaxaca.
+Día 6 
+• Desayuno en el hotel
+• Excursión a la zona arqueológica de Mitla y San María el Tule, sede de la, gran árbol infame Tule - el árbol más ancho del mundo.
+• Almuerzo en la Hacienda Zapata.
+• Visita a una Mezcal Palenque, para aprender acerca de la producción del mezcal.
+• Cena en Los Danzantes.
+Día 7
+• Desayuno en el hotel
+• Traslado al hotel.";
+$lang['inf_tour10mx']="El precio del tour no incluye  Tarifas aéreas y de aeropuerto Impuestos.
+• Bebidas y bebidas alcohólicas.
+• Gastos personales, comidas no incluidas en el itinerario, propinas y gratificaciones y otras guías turísticos independientes.";
+$lang['prec_tour10mx']="150 USD";
+
 
 
 ?>
