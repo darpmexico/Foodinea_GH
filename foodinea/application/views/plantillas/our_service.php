f8eeb7776296a887987df1f6686972fa19d9b991
alejandro pedraza <chno_k@hotmail.com>
2015-08-24 19:09:16 -0500

    Cambios en el index
    se agrego la seccion our service

---
+++ foodinea/application/views/plantillas/our_service.php
@@ -0,0 +1,103 @@
+ <!-- ***** Special services ***** -->
+        <section id="specialServices" class="special-services">
+
+            <div id="counterParallax" class="counter-parallax">
+
+                <div class="overlay overlay-black-70"></div>
+
+                <div class="container container-inner">
+
+                    <div class="quote text-center">
+                        <h1 class="white-color uppercase"><span class="light">Our</span> <span class="extra-bold">services</span></h1>
+                    </div>
+
+                    <div class="counters">
+
+                        <div class="col-sm-3 col-xs-6">
+                            <div class="counter text-center rotateInDownLeft-ifAppeared">
+                                <div class="number counter-number" id="counterNumber1">3</div>
+                                <p>Countries</p>
+                            </div>
+                        </div><!-- /.col-sm-3 -->
+
+                        <div class="col-sm-3 col-xs-6">
+                            <div class="counter text-center rotateInDownRight-ifAppeared">
+                                <div class="number counter-number" id="counterNumber2">50</div>
+                                <p>Satisfied Operators</p>
+                            </div>
+                        </div><!-- /.col-sm-3 -->
+
+                        <div class="col-sm-3 col-xs-6">
+                            <div class="counter text-center rotateInDownLeft-ifAppeared">
+                                <div class="number counter-number" id="counterNumber3">200</div>
+                                <p>Tours achieved</p>
+                            </div>
+                        </div><!-- /.col-sm-3 -->
+
+                        <div class="col-sm-3 col-xs-6">
+                            <div class="counter text-center rotateInDownRight-ifAppeared">
+                                <div class="number counter-number" id="counterNumber4">500</div>
+                                <p>Pleased clients</p>
+                            </div>
+                        </div><!-- /.col-sm-3 -->
+
+                    </div><!-- /#counters -->
+
+                </div><!-- /.container -->
+
+            </div><!-- /#counterParallax -->
+
+        </section><!-- /#specialServices -->
+
+        <section class="we-can-offer" id="weCanOffer">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-6">
+
+                        <h1 class="text-center"><span class="light">Why </span> <span class="extra-bold">us?</span></h1>
+<!--                         <div class="zigzag"><i class="fa fa-bell"></i></div> -->
+                        <p class="lead">Foodinea is the leading platform tours, attractions, activities for foodies, lifestyle and good living.</p>
+
+                    </div>
+
+                    <div class="col-md-6">
+
+                        <div class="offer-owl-carousel">
+
+                            <div class="item">
+                                <h3><i class="flaticon-circle flaticon-dark flaticon-covered16"></i> ATRACTIONS</h3>
+                                <p>Sed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urnaSed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urna</p>
+<!--                                 <a href="#" class="btn-more hvr-icon-forward">More</a> -->
+                            </div><!-- /.item -->
+
+                            <div class="item">
+                                <h3><i class="flaticon-circle flaticon-light flaticon-italian1"></i> QUALITY</h3>
+                                <p>Sed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urnaSed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urna</p>
+                               <!--  <a href="#" class="btn-more hvr-icon-forward">More</a> -->
+                            </div><!-- /.item -->
+
+                            <div class="item">
+                                <h3><i class="flaticon-circle flaticon-dark flaticon-soup12"></i> OFFERS</h3>
+                                <p>Sed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urnaSed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urna</p>
+                                <!-- <a href="#" class="btn-more hvr-icon-forward">More</a> -->
+                            </div><!-- /.item -->
+
+                            <div class="item">
+                                <h3><i class="flaticon-circle flaticon-light flaticon-drink24"></i> Beers</h3>
+                                <p>Sed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urnaSed cursus lectus ut lorem vehicula finibus. Maecenas vehicula sem at urna</p>
+<!--                                 <a href="#" class="btn-more hvr-icon-forward">More</a>
+ -->                            </div><!-- /.item -->
+
+
+                        </div><!-- /.offer-owl-carousel -->
+
+                    </div>
+
+                </div><!-- /.row -->
+
+            </div><!-- /.container -->
+
+        </section><!-- /#weCanOffer -->
